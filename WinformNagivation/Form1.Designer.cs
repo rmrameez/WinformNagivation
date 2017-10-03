dfe1a4af9e2eafb383c554baca06acf35a5309a9
Raja Muhammad Rameez <m_rameez@hotmail.com>
2017-10-03 15:21:27 +0500

    Initial commit

---
+++ WinformNagivation/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WinformNagivation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.content = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1270, 128);
+            this.panel1.TabIndex = 0;
+            // 
+            // content
+            // 
+            this.content.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.content.Location = new System.Drawing.Point(0, 128);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(1270, 504);
+            this.content.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::WinformNagivation.Properties.Resources.Shopping_Cart_96px;
+            this.button3.Location = new System.Drawing.Point(447, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 98);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::WinformNagivation.Properties.Resources.Product_96px;
+            this.button2.Location = new System.Drawing.Point(268, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 98);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WinformNagivation.Properties.Resources.Home_96px;
+            this.button1.Location = new System.Drawing.Point(89, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 98);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 632);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel content;
+    }
+}
+
---
+++ WinformNagivation/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformNagivation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            nav(home, content);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Products product = new Products();
+            nav(product, content);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Buy buy = new Buy();
+            nav(buy, content);
+        }
+
+        public void nav(Form form,Panel panel)
+        {
+            form.TopLevel = false;
+            form.Size = panel.Size; // for responsive size
+            panel.Controls.Clear();
+            panel.Controls.Add(form);
+            form.Show();
+        }
+    }
+}
